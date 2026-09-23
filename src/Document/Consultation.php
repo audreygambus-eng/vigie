9c3e9bf8acbe9eda000390d81156718ba3482b90
Audrey Gambus <audrey.gambus@gmail.com>
2026-09-23 18:52:42 +0200

    Ajout du document Consultation

---
+++ src/Document/Consultation.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Attribute as ODM;
+
+#[ODM\Document(collection: 'consultations')]
+class Consultation
+{
+    #[ODM\Id]
+    private ?string $id = null;
+
+    #[ODM\Field(type: 'int')]
+    private int $ressourceId;
+
+    #[ODM\Field(type: 'string')]
+    private string $titre;
+
+    #[ODM\Field(type: 'string')]
+    private string $categorie;
+
+    #[ODM\Field(type: 'date_immutable')]
+    private \DateTimeImmutable $dateConsultation;
+
+        public function __construct(
+        int $ressourceId,
+        string $titre,
+        string $categorie,
+        ?\DateTimeImmutable $dateConsultation = null,
+    ) {
+        $this->ressourceId = $ressourceId;
+        $this->titre = $titre;
+        $this->categorie = $categorie;
+        $this->dateConsultation = $dateConsultation ?? new \DateTimeImmutable();
+    }
+
+        public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getRessourceId(): int
+    {
+        return $this->ressourceId;
+    }
+
+    public function getTitre(): string
+    {
+        return $this->titre;
+    }
+
+    public function getCategorie(): string
+    {
+        return $this->categorie;
+    }
+
+    public function getDateConsultation(): \DateTimeImmutable
+    {
+        return $this->dateConsultation;
+    }
+}
