c7fc99fccdf4afa9567faca06356909c949d0b79
Audrey Gambus <audrey.gambus@gmail.com>
2026-09-25 13:25:26 +0200

    Test fonctionnel : création refusée sans authentification

---
+++ tests/Controller/RessourceControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RessourceControllerTest extends WebTestCase
+{
+    public function testCreationSansAuthentificationEstRefusee(): void
+    {
+        $client = static::createClient();
+
+        $client->request(
+            'POST',
+            '/api/ressources',
+            server: ['CONTENT_TYPE' => 'application/json'],
+            content: json_encode([
+                'titre' => 'Ressource de test',
+                'url' => 'https://exemple.fr',
+                'categorieId' => 1,
+            ]),
+        );
+
+        $this->assertResponseStatusCodeSame(401);
+        $this->assertResponseHasHeader('WWW-Authenticate');
+    }
+}
